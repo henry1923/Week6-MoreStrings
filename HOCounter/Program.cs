82f877f18f42671226b9fd06a794aa44e2f7e716
Opilane TTHK <opilane@tthk.ee>
2021-10-06 10:29:49 +0300

    first

---
+++ HOCounter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HOCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab mitu 'h', 'o', ja 'l' tähte on lauses "Hello, World!"
+
+            string helloW = "Hello World!".ToLower();
+
+            int counter1 = 0;
+            int counter = 0;
+            int counter2 = 0;
+            for (int i = 0; i < helloW.Length; i++)
+
+            {
+                if (helloW[i] == 'h')
+                {
+                    counterH++;
+                }
+                else if (helloW[i] == 'o')
+                {
+                    counterL++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Console.WriteLine($"Hello Worldis on {counter} 'h' {counter} 'o' ja {counter} 'l' tähte");
+        }
+    }
+}
